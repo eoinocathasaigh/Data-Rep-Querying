d5ace63b3a8cece9dd40b99ecc6d294d177de758
Eoin Ocathasaigh <G00417466@atu.ie>
2024-09-27 10:35:35 +0000

    2a & Lab Started

--- 2a.js
+++ 2a.js
@@ -1,4 +1,4 @@
-
+//Practice for this exercise
 let a = ()=>{
     console.log("Hello a");
 }
@@ -10,3 +10,11 @@ let b = (value)=>{
 }
 
 b("Bye");
+
+//2(a) Arrow function returning the appropriate message
+//Should return "Data Representation & Querying"
+let msg = ()=>{
+    console.log("Data Representation & Querying");
+}
+//Assigned variable calls and prints it to the console
+msg();
