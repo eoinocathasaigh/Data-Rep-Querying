c807facc447495e3ae081de3430f45605eb79166
Eoin Ocathasaigh <G00417466@atu.ie>
2024-09-27 10:46:08 +0000

    Finished 2(c) - Adding 2 passed variables together

---
+++ 2c.js
@@ -0,0 +1,7 @@
+//2(c) Returning sum of 2 arguments
+//Should receive 2 arguments and return their sum to the console
+let equal = (val1, val2)=>{
+    console.log("Value is: " + (val1 + val2));
+}
+//Passed values to the function
+equal(5, 5);
