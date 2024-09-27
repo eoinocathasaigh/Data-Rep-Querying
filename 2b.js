8db8dc4bfe96572145aecce7fe747ed3099b5701
Eoin Ocathasaigh <G00417466@atu.ie>
2024-09-27 10:40:47 +0000

    Question 2(b) finished - Passing a value to an arrow function and returning it to the console

---
+++ 2b.js
@@ -0,0 +1,7 @@
+//2(b) Extending previous function
+//Should return the value passed to it when called
+let val = (value)=>{
+    console.log(value);
+}
+//Passed variable should be called
+val("Variable is 45");
